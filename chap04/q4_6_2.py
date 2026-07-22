d14b02d04abba631c212e244b02f173c07fe7ecc
itc-n26015 <n26015@std.it-college.ac.jp>
2026-07-22 03:04:26 +0000

    4.6

--- chap04/q4_6_2.py
+++ chap04/q4_6_2.py
@@ -33,7 +33,7 @@ print('\nSort by height')
 for student in students_by_height:
     print(student)
 
-students_by_weight = sorted(students_dagta, key=lambda s: s[2])
+students_by_weight = sorted(students_data, key=lambda s: s[2])
 print('\nSort by weight')
 for student in students_by_weight:
     print(student)
